d53762a1ff2e2b47c09ee18168c62c625790b017
tiwobot <twogun.pre@gmail.com>
2023-07-27 02:47:19 +0300

    Add SharedPreferences to store basic data

---
+++ lib/user_preferences.dart
@@ -0,0 +1,49 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class UserPreferences {
+  late bool _isFirst;
+  late bool _isLoggedin;
+
+  initState() {
+    readIsFirst();
+    readIsLoggedin();
+  }
+
+  bool getIsFirst() {
+    return _isFirst;
+  }
+
+  bool getIsLoggedin() {
+    return _isLoggedin;
+  }
+
+  void setIsFirst(bool a) {
+    _isFirst = a;
+    writeIsFirst();
+  }
+
+  void setIsLoggedin(bool a) {
+    _isLoggedin = a;
+    writeIsLoggedin();
+  }
+
+  Future<void> readIsFirst() async {
+    final prefs = await SharedPreferences.getInstance();
+    _isFirst = prefs.getBool('isFirst')!;
+  }
+
+  Future<void> readIsLoggedin() async {
+    final prefs = await SharedPreferences.getInstance();
+    _isLoggedin = prefs.getBool('isLoggedin')!;
+  }
+
+  Future<void> writeIsFirst() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isFirst', _isFirst);
+  }
+
+  Future<void> writeIsLoggedin() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isLoggedin', _isLoggedin);
+  }
+}
