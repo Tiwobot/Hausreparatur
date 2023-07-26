977c331ccca696dfb4d654216ec2e1d9248a741d
tiwobot <twogun.pre@gmail.com>
2023-07-26 19:18:18 +0300

    Seperate main.dart

---
+++ lib/hausreparatur_init.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'login_page.dart';
+
+class HausReparatur extends StatelessWidget {
+  const HausReparatur({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      themeMode: ThemeMode.system,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'), // English
+        Locale('de'), // Deutsch
+      ],
+      home: const LoginPage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
