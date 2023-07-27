34f8d8ae1fffe49be7f0eaef857a58a5782b18f2
tiwobot <twogun.pre@gmail.com>
2023-07-27 14:41:49 +0300

    Force App to Portrait Mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'hausreparatur_init.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const HausReparatur());
 }
