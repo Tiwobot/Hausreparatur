f3703cdcf891732d744cb328d983e7112a4bcb00
tiwobot <twogun.pre@gmail.com>
2023-08-28 02:09:30 +0300

    Fix Next button

--- lib/create_account_page.dart
+++ lib/create_account_page.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class CreateAccountMail extends StatefulWidget {
-  const CreateAccountMail({super.key});
+class CreateAccountPage extends StatefulWidget {
+  const CreateAccountPage({super.key});
 
   @override
-  State<CreateAccountMail> createState() => _CreateAccountMailState();
+  State<CreateAccountPage> createState() => _CreateAccountPageState();
 }
 
-class _CreateAccountMailState extends State<CreateAccountMail> {
+class _CreateAccountPageState extends State<CreateAccountPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,31 +16,18 @@ class _CreateAccountMailState extends State<CreateAccountMail> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Image.asset(
-              'assets/images/icon_v3_transparent.png',
-              filterQuality: FilterQuality.medium,
-              height: 50,
-              width: 50,
-            ),
             Container(
-              margin: const EdgeInsets.fromLTRB(25, 0, 25, 35),
-              child: const Row(
+              margin: const EdgeInsets.fromLTRB(25, 0, 25, 20),
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Text(
-                    "Haus",
-                    style: TextStyle(
-                      fontFamily: 'Helvetica',
-                      fontSize: 30,
-                      color: Color(0xFFFFFFFF),
-                    ),
-                  ),
-                  Text(
-                    "reparatur",
-                    style: TextStyle(
-                      fontFamily: 'Helvetica',
-                      fontSize: 30,
-                      color: Color(0xFFF9A720),
+                    "Create Account",
+                    style: GoogleFonts.ubuntu(
+                      fontSize: 25,
+                      textStyle: const TextStyle(
+                        color: Color.fromARGB(255, 203, 203, 203),
+                      ),
                     ),
                   ),
                 ],
@@ -60,36 +47,17 @@ class _CreateAccountMailState extends State<CreateAccountMail> {
                 obscureText: false,
                 decoration: InputDecoration(
                   floatingLabelBehavior: FloatingLabelBehavior.always,
-                  hintText: "Email address or phone number",
+                  hintText: "Email address",
                   hintStyle: GoogleFonts.getFont("Ubuntu"),
                   border: InputBorder.none,
                 ),
               ),
             ),
             Container(
-              padding: const EdgeInsets.fromLTRB(15, 0, 15, 4),
-              margin: const EdgeInsets.fromLTRB(25, 0, 25, 10),
-              height: 50,
-              width: 370,
-              decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 52, 52, 52),
-                  border: Border.all(),
-                  borderRadius: const BorderRadius.all(Radius.circular(5))),
-              child: TextField(
-                style: GoogleFonts.getFont("Ubuntu"),
-                obscureText: false,
-                decoration: InputDecoration(
-                  floatingLabelBehavior: FloatingLabelBehavior.always,
-                  hintText: "Email address or phone number",
-                  hintStyle: GoogleFonts.getFont("Ubuntu"),
-                  border: InputBorder.none,
-                ),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.fromLTRB(25, 5, 25, 10),
-              height: 50,
-              width: 370,
+              padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+              margin: const EdgeInsets.fromLTRB(25, 5, 25, 20),
+              height: 60,
+              width: 100
               child: ElevatedButton(
                 style: ButtonStyle(
                   overlayColor: MaterialStateProperty.all(
@@ -109,68 +77,13 @@ class _CreateAccountMailState extends State<CreateAccountMail> {
                 onPressed: () {
                   //TODO: implement Login Button
                 },
-                child:
-                    const Text('Login', style: TextStyle(fontFamily: 'Ubuntu')),
+                child: const Icon(
+                  Icons.navigate_next_rounded,
+                  size: 35.0,
+                ),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Forgot your password?',
-                  style: GoogleFonts.ubuntu(
-                    textStyle: const TextStyle(
-                      color: Color.fromARGB(255, 145, 145, 145),
-                    ),
-                  ),
-                ),
-                InkWell(
-                  borderRadius: BorderRadius.circular(7),
-                  onTap: () {
-                    //TODO: implement Forget Password Button.
-                  },
-                  child: Container(
-                    margin: const EdgeInsets.fromLTRB(5, 0, 5, 0),
-                    child: Text(
-                      'Get Help',
-                      style: GoogleFonts.ubuntu(
-                        textStyle: const TextStyle(
-                          color: Color(0xFFFFFFFF),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
             ),
           ],
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        height: 70,
-        color: Colors.transparent,
-        elevation: 0,
-        child: InkWell(
-          borderRadius: BorderRadius.circular(20),
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => const CreateAccountPage()),
-            );
-          },
-          child: Container(
-            margin: const EdgeInsets.fromLTRB(0, 12, 0, 0),
-            child: Text(
-              'Create an account',
-              textAlign: TextAlign.center,
-              style: GoogleFonts.ubuntu(
-                textStyle: const TextStyle(
-                  color: Color.fromARGB(255, 203, 203, 203),
-                ),
-              ),
-            ),
-          ),
         ),
       ),
     );
