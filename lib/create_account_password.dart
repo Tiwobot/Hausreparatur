fc89bd8aabe81dfdf3a72c14dd59840e718cd2c6
tiwobot <twogun.pre@gmail.com>
2023-09-01 17:12:19 +0300

    Add email validator and create password page

--- lib/create_account_password.dart
+++ lib/create_account_password.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import 'create_account_password.dart';
-
-class CreateAccountPage extends StatefulWidget {
-  const CreateAccountPage({super.key});
+class CreateAccountPassword extends StatefulWidget {
+  const CreateAccountPassword({Key? key}) : super(key: key);
 
   @override
-  State<CreateAccountPage> createState() => _CreateAccountPageState();
+  State<CreateAccountPassword> createState() => _CreateAccountPasswordState();
 }
 
-class _CreateAccountPageState extends State<CreateAccountPage> {
+class _CreateAccountPasswordState extends State<CreateAccountPassword> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +22,7 @@ class _CreateAccountPageState extends State<CreateAccountPage> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   Text(
-                    "Create Account",
+                    "Set a Password",
                     style: GoogleFonts.ubuntu(
                       fontSize: 25,
                       textStyle: const TextStyle(
@@ -38,20 +36,19 @@ class _CreateAccountPageState extends State<CreateAccountPage> {
             Container(
               padding: const EdgeInsets.fromLTRB(15, 0, 15, 4),
               margin: const EdgeInsets.fromLTRB(25, 0, 25, 10),
-              height: 70,
+              height: 50,
               width: 370,
               decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 52, 52, 52),
-                  border: Border.all(),
-                  borderRadius: const BorderRadius.all(Radius.circular(5))),
-              child: TextFormField(
+                color: const Color.fromARGB(255, 52, 52, 52),
+                border: Border.all(),
+                borderRadius: const BorderRadius.all(Radius.circular(5)),
+              ),
+              child: TextField(
                 style: GoogleFonts.getFont("Ubuntu"),
-                autovalidateMode: AutovalidateMode.always,
-                validator: validateEmail,
-                obscureText: false,
+                obscureText: true,
                 decoration: InputDecoration(
                   floatingLabelBehavior: FloatingLabelBehavior.always,
-                  hintText: "Email address",
+                  hintText: "Password",
                   hintStyle: GoogleFonts.getFont("Ubuntu"),
                   border: InputBorder.none,
                 ),
@@ -79,11 +76,7 @@ class _CreateAccountPageState extends State<CreateAccountPage> {
                   ),
                 ),
                 onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const CreateAccountPassword()),
-                  );
+                  // TODO: Implement create account logic
                 },
                 child: const Icon(
                   Icons.navigate_next_rounded,
@@ -95,20 +88,5 @@ class _CreateAccountPageState extends State<CreateAccountPage> {
         ),
       ),
     );
-  }
-
-  String? validateEmail(String? value) {
-    const pattern = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'"
-        r'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-'
-        r'\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*'
-        r'[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4]'
-        r'[0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9]'
-        r'[0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\'
-        r'x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])';
-    final regex = RegExp(pattern);
-
-    return value!.isNotEmpty && !regex.hasMatch(value)
-        ? 'Enter a valid email address'
-        : null;
   }
 }
